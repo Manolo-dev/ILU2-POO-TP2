9ca85c4c10a657be189dd77e812ebf56e267490d
Manolo-dev <sardo.manolo.dev@gmail.com>
2024-04-05 14:30:54 +0200

    switch(IDE: Eclipse);

---
+++ src/controleur/ControlTrouverEtalVendeur.java
@@ -0,0 +1,21 @@
+package controleur;
+
+import villagegaulois.Etal;
+import villagegaulois.Village;
+import personnages.Gaulois;
+
+public class ControlTrouverEtalVendeur {
+	private Village village;
+
+	public ControlTrouverEtalVendeur(Village village) {
+		this.village = village;
+	}
+
+	public Etal trouverEtalVendeur(String nomVendeur) {
+		Gaulois vendeur = village.trouverHabitant(nomVendeur);
+		if(vendeur == null) {
+			return null;
+		}
+		return village.rechercherEtal(vendeur);
+	}
+}
