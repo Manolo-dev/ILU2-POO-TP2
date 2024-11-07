31a07cdd4ce82c58bd4c1066404f94146076f40c
Manolo-dev <sardo.manolo.dev@gmail.com>
2024-11-07 09:11:33 +0100

    2.7 Afficher Marche

--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -10,6 +10,27 @@ public class BoundaryAfficherMarche {
 	}
 
 	public void afficherMarche(String nomAcheteur) {
+		String[] infosMarche = controlAfficherMarche.donnerInfosMarche(nomAcheteur);
+		if(infosMarche.length == 0) {
+			System.out.println("Le marché est vide, revenez plus tard.");
+			return;
+		}
 
+		System.out.println(
+			  nomAcheteur
+			+ ", vous trouverez au marché :");
+		int i = 0;
+		while(i < infosMarche.length) {
+			String vendeur  = infosMarche[i++];
+			String quantite = infosMarche[i++];
+			String produit  = infosMarche[i++];
+			System.out.println(
+				  "- "
+				+ vendeur
+				+ " qui vend "
+				+ quantite
+				+ " "
+				+ produit);
+		}
 	}
 }
